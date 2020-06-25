17c4222caa7c2c3d73cf06b8e7df1f7da208bd3e
Azik-Gr <secbox8@gmail.com>
2020-06-25 20:16:15 +0300

    Задача N 4

--- src/Converter.java
+++ src/Converter.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Converter {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Input your number: ");
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
-        int decimalNumber = Integer.parseInt(number,2);
-        System.out.println(decimalNumber);
+        int decimalNumb = Integer.parseInt(number, 2);
+        System.out.println(decimalNumb);
     }
 
 }
