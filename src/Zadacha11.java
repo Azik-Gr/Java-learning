bac57f664283956752b20caed2354e12846058fd
Azik-Gr <secbox8@gmail.com>
2020-06-28 21:51:51 +0300

    Задача N 11

---
+++ src/Zadacha11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zadacha11 {
+    public static void main (String[] args){
+        System.out.println("Введите вашу строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("Вы ввели строку: "+text);
+    }
+}
+
