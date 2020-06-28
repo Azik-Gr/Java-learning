5536023e283cb1c6cfcb96c10ebeb52af1a9ead5
Azik-Gr <secbox8@gmail.com>
2020-06-28 22:06:19 +0300

    Задача N 12

--- src/Zadacha12.java
+++ src/Zadacha12.java
@@ -4,9 +4,9 @@ public class Zadacha12 {
     public static void main(String[] args) {
         System.out.println("Введите вашу строку: ");
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        text = text.replace(" ", "");
-        System.out.println("Получившийся результат: " + text);
+        String str = scanner.nextLine();
+        str = str.replace(" ", "");
+        System.out.println("Получившийся результат: " + str);
     }
 }
 
