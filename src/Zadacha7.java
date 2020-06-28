185da6ca7a9e135ffc1d96a1cfdc8609fcdb0e19
Azik-Gr <secbox8@gmail.com>
2020-06-28 21:00:36 +0300

    Задача N 7

--- src/Zadacha7.java
+++ src/Zadacha7.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Zadacha7 {
+    final static int X = 7;
+    final static int Y = 10;
+    final static int Z = 15;
     public static void main(String[] args) {
-        int x = 7;
-        int y = 15;
-        int z = 10;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите ваше число: ");
         int number = scanner.nextInt();
-        if (number == x || number == y || number == z) {
+        if (number == X || number == Y || number == Z) {
             System.out.println("Данное значение имеется в константах");
         } else System.out.println("Такой константы нет!");
     }
